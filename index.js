007c7d502ddc7a37131762d4f52084995fcbd131
Kutsikas27 <mardus80@gmail.com>
2024-09-03 20:26:43 +0300

    fix the month calucation

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const salary = document.getElementById("salary");
 const output = document.getElementById("output");
 const calculateDaysUntilPayday = () => {
   const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+
   let nextMonth = currentDate.getMonth() + 1;
   let year = currentDate.getFullYear();
+
+  if (currentDay <= 9) {
+    nextMonth = currentDate.getMonth();
+  }
   if (nextMonth > 11) {
     nextMonth = 0;
     year += 1;
   }
+
   const nextMonthNinth = new Date(year, nextMonth, 9);
   const timeDifference = nextMonthNinth - currentDate;
   const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
   return daysDifference;
 };
-
 salary.addEventListener("input", (event) => {
   output.innerHTML = `Palgapäevani on ${calculateDaysUntilPayday()} päeva, päevas saad kulutada ${(
     salary.value / calculateDaysUntilPayday()
